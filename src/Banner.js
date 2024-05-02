2e04dd5d0682b4c1552e3dc75386f1fc9a83f306
Liam Walton <78359715+LiamWalt0n@users.noreply.github.com>
2024-05-02 22:55:22 +0100

    Add files via upload
    missing node_modules for now

---
+++ src/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Banner.css';
+
+function Banner() {
+  return <div className='banner' 
+  style ={{
+    backgroundSize: 'cover',
+    backgroundImage: 'url("https://thelesfilms.files.wordpress.com/2016/06/netflix-banner.jpg")',
+    backgroundPosition: "center center",
+  }}>
+
+
+    </div>;
+
+}
+
+export default Banner
+
+// Time on vid is 36:36
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNNF4F7zOf55jKqnPg96rVREt9CHGsUSY",
+    authDomain: "netflix-clone-github-66023.firebaseapp.com",
+    projectId: "netflix-clone-github-66023",
+    storageBucket: "netflix-clone-github-66023.appspot.com",
+    messagingSenderId: "100756116685",
+    appId: "1:100756116685:web:2ccbe2f8aa3dbdc4820584"
+  };
